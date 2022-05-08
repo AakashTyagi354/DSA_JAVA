4ad46627c973d2beb21eff35d2bc1f0d2eb82c57
Aakash Tyagi <78840211+AakashTyagi354@users.noreply.github.com>
2022-05-08 09:37:18 +0530

    get started

---
+++ arrays/shuffle.java
@@ -0,0 +1,7 @@
+package arrays;
+
+public class shuffle {
+    public static void main(String[] args) {
+        System.out.println("hello java");
+    }
+}
---
+++ searching/binarySearch.java
@@ -0,0 +1,7 @@
+package searching;
+
+public class binarySearch {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
